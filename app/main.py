5751756f9f70bfa624cffccdfad7da61f23749c4
Viktor <Viktordikarev1992@yandex.ru>
2026-02-14 23:00:50 +0400

    processingplan

--- app/main.py
+++ app/main.py
@@ -1,1 +1,15 @@
-print ('Hello world')
+from pprint import pprint
+import requests
+
+from app.config import settings
+
+token = settings.TOKEN
+
+headers = {
+    'Authorization': f'Bearer {token}',
+    'Accept-Encoding': 'gzip'
+}
+
+response = requests.get('https://api.moysklad.ru/api/remap/1.2/entity/processingplan',
+                        headers=headers)
+pprint(len(response.json()))
